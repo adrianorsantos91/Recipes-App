3660d5d06ceb20cccd3bdc11110d748de48471c5
leonardo-terra <leo.terra.melo@gmail.com>
2022-03-28 17:27:57 -0300

    fix: layout issues

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ function Login() {
   }
 
   return (
-    <>
+    <div className="meals">
       <span className="logo">TRYBE</span>
       <object
         className="rocksGlass"
@@ -27,47 +27,40 @@ function Login() {
         Glass
       </object>
 
-      <div className="form-container">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            data-testid="email-input"
+            type="email"
+            placeholder="Enter email"
+            onChange={ (event) => setEmail(event.target.value) }
+            value={ email }
+          />
+        </Form.Group>
 
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              data-testid="email-input"
-              type="email"
-              placeholder="Enter email"
-              onChange={ (event) => setEmail(event.target.value) }
-              value={ email }
-            />
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            data-testid="password-input"
+            type="password"
+            placeholder="Password"
+            onChange={ (event) => setPassword(event.target.value) }
+            value={ password }
+          />
+        </Form.Group>
+        <Button
+          variant="primary"
+          type="button"
+          data-testid="login-submit-btn"
+          disabled={ (password.length < minPasswordLength) || !(validateEmail(email)) }
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              data-testid="password-input"
-              type="password"
-              placeholder="Password"
-              onChange={ (event) => setPassword(event.target.value) }
-              value={ password }
-            />
-          </Form.Group>
-
-          <Button
-            variant="primary"
-            type="button"
-            data-testid="login-submit-btn"
-            disabled={ (password.length < minPasswordLength) || !(validateEmail(email)) }
-
-          >
-            Login
-          </Button>
-        </Form>
-      </div>
-    </>
+        >
+          Login
+        </Button>
+      </Form>
+    </div>
   );
 }
 
 export default Login;
-/*  <div className="meals">
-
-  </div> */
