f70f4031deb1731a979df05430a9d8a07074c4a9
leonardo-terra <leo.terra.melo@gmail.com>
2022-03-29 17:36:48 -0300

    dev: render drink cards - only 12, as requirement

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import { Footer, Header } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import { Header, Footer } from '../components';
+import './Foods.css';
+import '../App.css';
 
 export default function Drinks() {
+  const [data, setData] = useState([]);
+
+  const exhibitionNumber = 12;
+
+  useEffect(() => {
+    const URL_NAME = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+    const fetchName = async () => {
+      const { drinks } = await fetch(URL_NAME).then((response) => response.json());
+      setData(drinks);
+    };
+    fetchName();
+  }, []);
+  console.log(data);
+
   return (
     <div>
-      <Header title="Drinks" hasSearch />
+      <div>
+        <Header title="Drinks" hasSearch />
+        {data.filter((_item, index) => index < exhibitionNumber).map((drink) => (
+          <div key={ drink.idDrink } className="container">
+            <Card style={ { width: '18rem' } }>
+              <Card.Img variant="top" src={ drink.strDrinkThumb } />
+              <Card.Body>
+                <Card.Title>{drink.strDrink}</Card.Title>
+                <Button variant="primary">See more</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
       <Footer />
     </div>
   );
