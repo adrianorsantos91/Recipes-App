59d7dc5e8b76e84fe0c930b26fff4752f2c46f6d
Rafael <rafael.fcardoso@hotmail.com>
2022-03-29 18:56:51 -0300

    Feat: 83 and 87 requirements.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Footer, Header } from '../components';
 
-const Profile = () => (
-  <>
-    <Header title="Profile" />
-    <h2 data-testid="profile-email">email@email.com</h2>
-    <Link to="/done-recipes">
-      <button type="button" data-testid="profile-done-btn">
-        Done Recipes
-      </button>
-    </Link>
-    <Link to="/favorite-recipes">
-      <button type="button" data-testid="profile-favorite-btn">
-        Favorite Recipes
-      </button>
-    </Link>
-    <button type="button" data-testid="profile-logout-btn">
-      Logout
-    </button>
-    <Footer />
-  </>
-);
+const Profile = () => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const userEmail = (JSON.parse(localStorage.getItem('user')));
+    setEmail(userEmail.email);
+  }, []);
+
+  return (
+    <>
+      <Header title="Profile" />
+      <h2 data-testid="profile-email">
+        {email}
+      </h2>
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">
+          Done Recipes
+        </button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button type="button" data-testid="profile-favorite-btn">
+          Favorite Recipes
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => localStorage.clear() }
+        >
+          Logout
+        </button>
+      </Link>
+      {}
+      <Footer />
+    </>
+  );
+};
 
 export default Profile;
