78f61a07b2c33830409b62235c9424aafbba7f6f
Rafael <rafael.fcardoso@hotmail.com>
2022-03-29 17:30:22 -0300

    Feat: 82 requirement

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,16 @@ import { Footer, Header } from '../components';
 const Profile = () => (
   <>
     <Header title="Profile" />
+    <h2 data-testid="profile-email">email@email.com</h2>
+    <button type="button" data-testid="profile-done-btn">
+      Done Recipes
+    </button>
+    <button type="button" data-testid="profile-favorite-btn">
+      Favorite Recipes
+    </button>
+    <button type="button" data-testid="profile-logout-btn">
+      Logout
+    </button>
     <Footer />
   </>
 );
