af6f2615fa3952e79657865cf6690ed16ea84797
leonardo-terra <leo.terra.melo@gmail.com>
2022-03-29 16:56:50 -0300

    dev: save food data in state

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import login from './loginReducer';
+import foodData from './foodAPIData';
 
 const rootReducer = combineReducers({
   login,
+  foodData,
 });
 
 export default rootReducer;
