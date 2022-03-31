b6bfdc976968f920ee10b879b443b4b5c10867ae
Rafael <rafael.fcardoso@hotmail.com>
2022-03-31 14:23:04 -0300

    Dev: requirement 54, adds page elements

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
+import { Card, Button } from 'react-bootstrap';
 import { Header } from '../components';
 
 const DoneRecipes = () => (
   <>
     <Header title="Done Recipes" />
-    <button
-      type="button"
-      data-testid="filter-by-all-btn"
-    >
-      All
-    </button>
-    <button
-      type="button"
-      data-testid="filter-by-food-btn"
-    >
-      Foods
-    </button>
-    <button
-      type="button"
-      data-testid="filter-by-drink-btn"
-    >
-      Drinks
-    </button>
+    <div>
+      <button // Botoes de Filtro
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-food-btn"
+      >
+        Foods
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+    </div>
+
+    <Card style={ { width: '18rem' } }>
+      <Card.Img
+        data-testid={ `${index}-horizontal-image` }
+        variant="top"
+        src={ recipe.strMealThumb }
+      />
+      <Card.Body>
+        <p // <--------------Categoria
+          data-testid={ `${index}-horizontal-top-text` }
+        >
+          Categoria
+        </p>
+        <Card.Title // <-----Nome da Receita
+          data-testid={ `${index}-horizontal-name` }
+        >
+          {recipe.strMeal}
+
+        </Card.Title>
+        <Card.Text // <-----Data da Receita
+          data-testid={ `${index}-horizontal-done-date` }
+        >
+          {recipe.strMeal}
+
+        </Card.Text>
+        <Button // <-----Botao Share
+          variant="primary"
+          data-testid={ `${index}-horizontal-share-btn` }
+        >
+          Share
+        </Button>
+        <Card.Link
+          href="#"
+          data-testid={ `${index}-${tagName}-horizontal-tag` }
+        >
+          Card Link
+        </Card.Link>
+      </Card.Body>
+    </Card>
   </>
 );
 
