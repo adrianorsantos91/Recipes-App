1b9906e3b1dd04c4201aa97a7a0dc8fd65283033
Adriano Santos <adriano.santosars@outlook.com>
2022-03-31 17:08:31 -0300

    fix: resolve conflits

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,27 +2,21 @@ import { combineReducers } from 'redux';
 import login from './loginReducer';
 import foodData from './foodAPIData';
 import drinkData from './drinkAPIData';
-<<<<<<< HEAD
 import foodDataDetails from './foodDetailsAPIData';
 import drinkDataDetails from './drinkDetailsAPIData';
 import drinksRecommendation from './drinksRecommendationAPIData';
 import foodsRecommendation from './foodsRecommendationAPIData';
-=======
 import categoryFoodData from './foodCategoryAPIData';
->>>>>>> 4d6d14edbd0fd33bc30c19e1ccce7b20655fb46e
 
 const rootReducer = combineReducers({
   login,
   foodData,
   drinkData,
-<<<<<<< HEAD
   foodDataDetails,
   drinkDataDetails,
   drinksRecommendation,
   foodsRecommendation,
-=======
   categoryFoodData,
->>>>>>> 4d6d14edbd0fd33bc30c19e1ccce7b20655fb46e
 });
 
 export default rootReducer;
