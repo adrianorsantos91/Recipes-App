99d5f0bc1fce8d6ef2590056ff1185bfb15ad836
Douglas Marcal <douglasymarcal@gmail.com>
2022-04-05 19:40:20 -0300

    dev: tests `RecipeInProgress` page

--- src/__tests__/RecipeInProgress.test.js
+++ src/__tests__/RecipeInProgress.test.js
@@ -113,4 +113,96 @@ describe('Testa local storage', () => {
     expect(newLocalStorage.meals['53026'].length).not.toBe(2);
     expect(newLocalStorage.meals['53026'].length).toBe(1);
   });
+
+  test('Verifica se ao carregar a página carrega bebida salva', async () => {
+    localStorage.clear();
+    const favoriteRecipe = [{
+      id: '13332',
+      type: 'drink',
+      nationality: '',
+      category: 'Shot',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'B-53',
+      image: 'https://www.thecocktaildb.com/images/media/drink/rwqxrv1461866023.jpg',
+    }];
+
+    const inProgressRecipe = {
+      cocktails: {
+        13332: [
+          'Kahlua',
+        ],
+      },
+      meals: {},
+    };
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipe));
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipe));
+
+    const { history } = renderWithRedux(<App />);
+    history.push('/drinks/13332/in-progress');
+
+    const favoriteButton = await screen
+      .findByRole('img', { name: /black heart favorite icon/i });
+    expect(favoriteButton).toBeInTheDocument();
+  });
+
+  test('Verifica se adiciona mais de uma receita no local storage', async () => {
+    localStorage.clear();
+    const favoriteRecipe = [
+      {
+        id: '53013',
+        type: 'food',
+        nationality: 'American',
+        category: 'Beef',
+        alcoholicOrNot: '',
+        name: 'Big Mac',
+        image: 'https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg',
+      },
+    ];
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipe));
+
+    const { history } = renderWithRedux(<App />);
+    history.push('/foods/52978/in-progress');
+
+    const favoriteButton = await screen
+      .findByRole('img', { name: /white heart favorite icon/i });
+    userEvent.click(favoriteButton);
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    expect(favoriteRecipes.length).toBe(2);
+  });
+
+  test('Verifica se ao carregar a página carrega receita salva', async () => {
+    localStorage.clear();
+    const favoriteRecipe = [
+      {
+        id: '53013',
+        type: 'food',
+        nationality: 'American',
+        category: 'Beef',
+        alcoholicOrNot: '',
+        name: 'Big Mac',
+        image: 'https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg',
+      },
+    ];
+
+    const inProgressRecipe = {
+      cocktails: {},
+      meals: {
+        53013: [
+          'Olive Oil',
+        ],
+      },
+    };
+
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipe));
+    localStorage.setItem('inProgressRecipes', JSON.stringify(inProgressRecipe));
+
+    const { history } = renderWithRedux(<App />);
+    history.push('/foods/53013/in-progress');
+
+    const favoriteButton = await screen
+      .findByRole('img', { name: /black heart favorite icon/i });
+    expect(favoriteButton).toBeInTheDocument();
+  });
 });
