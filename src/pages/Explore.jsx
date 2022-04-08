0601094198c0539476c633dc79bb6cd006e70a06
Rafael <rafael.fcardoso@hotmail.com>
2022-04-08 13:59:28 -0300

    dev: imports new icons

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Footer, Header } from '../components';
+import '../App.css';
 
 export default function Explore() {
   return (
-    <div>
-      <Header title="Explore" />
+    <div className="background">
+      <Header title="Explore" hasSearch />
       <Link to="/explore/foods">
         <Button
           variant="outline-dark"
