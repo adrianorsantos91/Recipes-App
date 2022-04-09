a0f56f9790c3007ce1ddc2fa2c6f6a3a9799f201
Rafael <rafael.fcardoso@hotmail.com>
2022-04-09 10:18:04 -0300

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,34 +6,34 @@ import meal from '../images/restaurant.svg';
 import '../styles/Footer.css';
 
 const Footer = () => (
-  <footer data-testid="footer">
+  <footer data-testid="footer" className="link">
     <Link to="/drinks">
       <img
         src={ drink }
         className="drinks"
         alt="drinks"
-        width="50"
-        height="50"
+        width="30"
+        height="30"
         data-testid="drinks-top-btn"
       />
     </Link>
-    <Link to="/explore">
+    <Link to="/explore" className="link">
       <img
         src={ explore }
         className="explore"
         alt="Icon Explore"
-        width="50"
-        height="50"
+        width="30"
+        height="30"
         data-testid="explore-bottom-btn"
       />
     </Link>
-    <Link to="/foods">
+    <Link to="/foods" className="link">
       <img
         src={ meal }
         className="explore"
         alt="Icon Meal"
-        width="50"
-        height="50"
+        width="30"
+        height="30"
         data-testid="food-bottom-btn"
       />
     </Link>
