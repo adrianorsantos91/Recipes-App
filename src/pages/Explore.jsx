44f4b5329a99256aa9797bd5685fd981f85a455f
Rafael <rafael.fcardoso@hotmail.com>
2022-04-10 20:32:53 -0300

    feat: Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Footer, Header } from '../components';
 import '../App.css';
+import '../styles/Explore.css';
 
 export default function Explore() {
   return (
     <div className="background">
       <Header title="Explore" hasSearch />
-      <Link to="/explore/foods">
-        <Button
-          variant="outline-dark"
-          data-testid="explore-foods"
-        >
-          Explore Foods
-        </Button>
-      </Link>
-      <Link to="/explore/drinks">
-        <Button
-          variant="outline-dark"
-          data-testid="explore-drinks"
-        >
-          Explore Drinks
-        </Button>
-      </Link>
+      <div className="container-explore">
+        <Link to="/explore/foods">
+          <button
+            type="button"
+            variant="outline-dark"
+            data-testid="explore-foods"
+            className="button"
+          >
+            Explore Foods
+          </button>
+        </Link>
+        <Link to="/explore/drinks">
+          <button
+            type="button"
+            variant="outline-dark"
+            data-testid="explore-drinks"
+            className="button"
+          >
+            Explore Drinks
+          </button>
+        </Link>
+      </div>
+
       <Footer />
     </div>
   );
