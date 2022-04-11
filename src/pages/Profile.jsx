b02169931607c920444809749be40c43990eb9ba
Rafael <rafael.fcardoso@hotmail.com>
2022-04-11 10:39:46 -0300

    feat: Profile Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import { Footer, Header } from '../components';
+import '../App.css';
+import '../styles/Profile.css';
 
 const Profile = () => {
   const [email, setEmail] = useState('');
@@ -11,33 +13,35 @@ const Profile = () => {
   }, []);
 
   return (
-    <>
+    <div className="background">
       <Header title="Profile" />
-      <h2 data-testid="profile-email">
-        {email}
-      </h2>
-      <Link to="/done-recipes">
-        <button type="button" data-testid="profile-done-btn">
-          Done Recipes
-        </button>
-      </Link>
-      <Link to="/favorite-recipes">
-        <button type="button" data-testid="profile-favorite-btn">
-          Favorite Recipes
-        </button>
-      </Link>
-      <Link to="/">
-        <button
-          type="button"
-          data-testid="profile-logout-btn"
-          onClick={ () => localStorage.clear() }
-        >
-          Logout
-        </button>
-      </Link>
-      {}
+      <div className="container-profile">
+        <h2 className="email" data-testid="profile-email">
+          {email}
+        </h2>
+        <Link to="/done-recipes">
+          <button type="button" data-testid="profile-done-btn" className="button">
+            Done Recipes
+          </button>
+        </Link>
+        <Link to="/favorite-recipes">
+          <button type="button" data-testid="profile-favorite-btn" className="button">
+            Favorite Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+            className="button"
+          >
+            Logout
+          </button>
+        </Link>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
